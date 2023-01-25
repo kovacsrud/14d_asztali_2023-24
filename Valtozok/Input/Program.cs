0d8840baf62f901592e359321cc8174eaa192e92
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-01-25 11:37:42 +0100

    Változók, input

---
+++ Valtozok/Input/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Felhasználói input
+            Console.Write("Adjon meg egy nevet!");
+            string nev = Console.ReadLine();
+
+            Console.WriteLine($"A megadott név:{nev}");
+
+            //Számok bekérése
+            int szam1 = 0;
+            int szam2 = 0;
+
+            Console.Write("Add meg szam1-et:");
+            szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Add meg szam2-t:");
+            //szam2 = Convert.ToInt32(Console.ReadLine());
+            szam2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(szam1-szam2);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Valtozok/Valtozok/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valtozok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asztali alkalmazások.");
+            Console.Write("Egy sorba");
+            Console.Write(" íródik");
+            Console.WriteLine();
+
+            //Egész típusú változók
+            //Több ilyen is van, előjeles ill. előjel nélküli, több méret
+
+            int a = 178;
+            int b = 56;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a+b);
+
+            Console.WriteLine($"A:{a},B:{b},összeg:{a+b}");
+
+            short c = 34;
+            long d = 234234255;
+
+            Console.WriteLine($"8 bites: min:{Byte.MinValue},max:{Byte.MaxValue}");
+            Console.WriteLine($"16 bites: min:{Int16.MinValue},max:{Int16.MaxValue}");
+            Console.WriteLine($"16 bites: min:{UInt16.MinValue},max:{UInt16.MaxValue}");
+            Console.WriteLine($"32 bites: min:{Int32.MinValue},max:{Int32.MaxValue}");
+            Console.WriteLine($"32 bites: min:{UInt32.MinValue},max:{UInt32.MaxValue}");
+            Console.WriteLine($"64 bites: min:{Int64.MinValue},max:{Int64.MaxValue}");
+            Console.WriteLine($"64 bites: min:{UInt64.MinValue},max:{UInt64.MaxValue}");
+
+            //Lebegőpontos számok (nem egész)
+            float szam1 = 12.123456789012345678901234567890f;
+            double szam2 = 12.123456789012345678901234567890;
+            decimal szam3 = 12.123456789012345678901234567890m;
+
+            
+            Console.WriteLine($"Float:{szam1}");
+            Console.WriteLine($"Double:{szam2}");
+            Console.WriteLine($"Decimal:{szam3}");
+
+
+            //Logikai érték:
+            bool logikai = true;
+            Console.WriteLine(logikai);
+            logikai = !logikai;
+            Console.WriteLine(logikai);
+            logikai = !logikai;
+            Console.WriteLine(logikai);
+
+            //Szöveges változó
+            string szoveg = "valami szöveg";
+
+            Console.WriteLine(szoveg);
+            //A string típus értéke nem változtatható meg (immutable).
+            szoveg = "valami más szöveg";
+            Console.WriteLine(szoveg);
+            Console.WriteLine(szoveg[3]);
+
+            //Karakteres típus (egyetlen betű)
+
+            char betu = 'a';
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
