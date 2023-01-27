1d2022cc280dd6a12ddb116d3eca8c276117def9
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-01-27 09:24:33 +0100

    Tömbök

---
+++ Tombok/ElemiAlgoritmusok/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElemiAlgoritmusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            //Elemi algoritmusok
+            int[] szamok = new int[100];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rand.Next(-200,200+1);
+            }
+
+            //Megszámlálás
+            int negativok = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if (szamok[i]<0)
+                {
+                    negativok++;
+                }
+            }
+
+            Console.WriteLine($"Negatív elemek száma:{negativok},Pozitívok:{szamok.Length-negativok}");
+
+            //Összegzés
+            int osszeg = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                osszeg += szamok[i];
+            }
+
+            Console.WriteLine($"Összeg:{osszeg}");
+
+            //Minimum és maximum kiválasztás
+            int min = szamok[0];
+            int max = szamok[0];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if (szamok[i]<min)
+                {
+                    min = szamok[i];
+                }
+                if (szamok[i]>max)
+                {
+                    max = szamok[i];
+                }
+            }
+
+            Console.WriteLine($"Min:{min},Max:{max}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tombok/Tombok/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tombok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tömb:azonos típusú adatokat tároló adatszerkezet. Deklaráláskor
+            //meg kell adni az elemek számát.
+            Random rand = new Random();
+
+            int[] szamok = new int[5];
+            int[] szamok2 = { 12, 36, 778, 39, 13, 31 };
+
+
+            //Indexelés: a tömb elemeire a sorszámukkal (index) lehet hivatkozni.
+            Console.WriteLine(szamok[1]);
+            Console.WriteLine(szamok[4]);
+            Console.WriteLine(szamok2[2]);
+            szamok2[2] = 399;
+            Console.WriteLine(szamok2[2]);
+
+            //Tömb feldolgozása:ciklus
+            for (int i = 0; i < szamok2.Length; i++)
+            {
+                Console.Write(szamok2[i]+" ");
+            }
+            Console.WriteLine();
+
+            //Foreach ciklus, ebben a ciklusban nem lehet a tömb elemeit módosítani!
+            //A tömb elemeit módosítani csak a for ciklusban lehet.
+            foreach (var i in szamok2)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rand.Next(10, 100 + 1);
+                Console.WriteLine(szamok[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
