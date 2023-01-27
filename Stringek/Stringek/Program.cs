6e7b78f66a855648cc807324cce70b6b9fa150d4
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-01-27 10:45:19 +0100

    Stringek

---
+++ Stringek/Stringek/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string szoveg = "Valami szöveg";
+            //Gyakorlatilag tömb, nem lehet megváltoztatni.
+            //Tömb: az elemszám kötött.
+
+            for (int i = 0; i < szoveg.Length; i++)
+            {
+                Console.WriteLine(szoveg[i]);
+            }
+
+            szoveg = "Valami szöveg és még szöveg";
+
+            //String műveletek
+            //Kisbetűsre alakítás
+            Console.WriteLine(szoveg.ToLower());
+            
+            
+            Console.WriteLine(szoveg);
+            Console.ReadKey();
+        }
+    }
+}
