7439bfb43178b9981aab6a2edbf07e61b1b7bb15
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-02-06 09:09:02 +0100

    Karakter manipuláció

---
+++ KarakterManipulacio/KarakterManipulacio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterManipulacio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Karakterek manipulációja
+
+            string szoveg = "vALAmi sZövEg";
+            Console.WriteLine(szoveg);
+
+            char[] szovegTomb = szoveg.ToCharArray();
+
+
+            for (int i = 0; i < szovegTomb.Length; i++)
+            {
+                if (Char.IsLower(szoveg[i]))
+                {
+                    szovegTomb[i] = Char.ToUpper(szovegTomb[i]);
+                } else
+                {
+                    szovegTomb[i] = Char.ToLower(szovegTomb[i]);
+                }
+            }
+
+            
+
+            szoveg = new string(szovegTomb);
+
+            Console.WriteLine(szoveg);
+
+            Console.ReadKey();
+        }
+    }
+}
