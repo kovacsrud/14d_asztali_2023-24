b00f449f4d7fc40ee1259ddf99daeadfb580bfdf
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-02-07 08:21:20 +0100

    Karakter numerikus értéke

--- KarakterManipulacio/KarakterManipulacio/Program.cs
+++ KarakterManipulacio/KarakterManipulacio/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +35,23 @@ namespace KarakterManipulacio
             szoveg = new string(szovegTomb);
 
             Console.WriteLine(szoveg);
+
+            string masikSzoveg = "Valami 1234";
+
+            char[] masikSzovegArray = masikSzoveg.ToCharArray();
+
+            int osszeg = 0;
+            for (int i = 0; i < masikSzovegArray.Length; i++)
+            {
+                if (Char.IsNumber(masikSzovegArray[i]))
+                {
+                    osszeg += (int)Char.GetNumericValue(masikSzovegArray[i]);
+                    Debug.WriteLine(osszeg);
+                    //Debug.WriteLine(masikSzovegArray[i]);
+                }
+            }
+
+            Console.WriteLine(osszeg);
 
             Console.ReadKey();
         }
