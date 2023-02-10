87ac08107c67096de2200484894999ec64224c44
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-02-10 12:14:01 +0100

    OOp alapok

---
+++ OOPAlapok/OOPAlapok/Ember.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAlapok
+{
+    class Ember
+    {
+        //Az osztály adatokat (változók,adattag) és műveleteket
+        //(metódus, függvény) tartalmazhat.
+
+       //Az oop egyik alapelve a bezártság (encapsulation)
+       //Az osztály egy interfészen keresztül lehessen elérhető
+        
+       private string nev;
+       private string szuletesiHely;
+       private int magassag;
+       private int suly;
+
+        //Konstruktor függvény: az osztály példányosításakor 
+        //automatikusan lefut
+        //Nincs visszatérési értéke, és void sem lehet, a nevének meg kell 
+        //egyeznie az osztály nevével
+        public Ember(string nev,string szuletesiHely,int magassag,int suly)
+        {
+            this.nev = nev;
+            this.szuletesiHely = szuletesiHely;
+            this.magassag = magassag;
+            this.suly = suly;
+        }
+
+        public Ember()
+        {
+                
+        }
+
+        public string GetNev()
+        {
+            return nev;
+        }
+
+        public void SetNev(string nev)
+        {
+
+            this.nev = nev;
+        }
+
+        public void SetMagassag(int magassag)
+        {
+            if (magassag<80)
+            {
+                throw new ArgumentException("Hibás érték");
+            } else
+            {
+                this.magassag = magassag;
+            }
+        }
+
+        public int GetMagassag()
+        {
+            return magassag;
+        }
+
+    }
+}
---
+++ OOPAlapok/OOPAlapok/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAlapok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Példányosítás
+            Ember ubul = new Ember("Ubul","Kaba",187,87);
+            Ember elek = new Ember("Elek","Szeged",176,78);
+            Ember geza = new Ember();
+
+            ubul.SetNev("Ubul");
+            ubul.SetMagassag(190);
+
+            Console.WriteLine(ubul.GetNev());
+            Console.WriteLine(ubul.GetMagassag());
+
+            //ubul.nev = "Ubul";
+            //ubul.magassag = 176;
+            //ubul.suly = 69;
+            //ubul.szuletesiHely = "Kaba";
+
+            //Console.WriteLine(ubul.nev);
+
+
+            Console.ReadKey();
+        }
+    }
+}
