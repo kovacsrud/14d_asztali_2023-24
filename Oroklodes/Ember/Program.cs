83746e5fb1f6f5a8c656d6ac6072c3f0300201b7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-02-23 09:07:44 +0100

    Update Program.cs

--- Oroklodes/Ember/Program.cs
+++ Oroklodes/Ember/Program.cs
@@ -10,7 +10,18 @@ namespace Ember
     {
         static void Main(string[] args)
         {
+
+            List<Sportolo> sportolok = new List<Sportolo>();
+            List<Nyugdijas> nyugdijasok = new List<Nyugdijas>();
+
+            List<Ember> emberek = new List<Ember>();
+
             Sportolo sportolo = new Sportolo("triatlon", "Elek", 81, 180, 2003);
+
+            sportolok.Add(sportolo);
+            emberek.Add(sportolo);
+
+
 
             sportolo.Mozog();
             sportolo.Sportol();
@@ -19,9 +30,14 @@ namespace Ember
 
             Nyugdijas nyugdijas = new Nyugdijas("Iván", 92, 186, 1951, "mérnök");
 
+            emberek.Add(nyugdijas);
+
             Console.WriteLine(nyugdijas.Eletkor());
             nyugdijas.Mozog();
             Console.WriteLine(nyugdijas.ToString());
+
+
+            Console.WriteLine(emberek[0].Eletkor());
 
 
             Console.ReadKey();
