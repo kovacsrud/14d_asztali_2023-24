c3d4500963be44aab2b3a3845ffa8976df34ce9b
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-02-24 13:25:52 +0100

    SzamitogepOOP

---
+++ Szamitogep/Szamitogep/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamitogep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Szamitogep gep1 = new Szamitogep();
+            Szamitogep gep2 = new Szamitogep(2048,false);
+
+            gep1.Kapcsol();
+            Console.WriteLine(gep1.Masolas(800));
+            Console.WriteLine(gep1.Masolas(400));
+            Console.WriteLine(gep1.ToString());
+            Console.WriteLine(gep2.ToString());
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Szamitogep/Szamitogep/Szamitogep.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamitogep
+{
+    class Szamitogep
+    {
+        private double szabadMemoria;
+        private bool bekapcsolva;
+
+        public Szamitogep()
+        {
+            szabadMemoria = 1024;
+            bekapcsolva = false;
+        }
+
+        public Szamitogep(double szabadmemoria,bool bekapcsolva)
+        {
+            szabadMemoria = szabadmemoria;
+            this.bekapcsolva = bekapcsolva;
+        }
+
+        public void Kapcsol()
+        {
+            bekapcsolva = !bekapcsolva;
+        }
+
+        public bool Masolas(double programMeret)
+        {
+            bool sikeresMasolas = false;
+            if (programMeret<szabadMemoria && bekapcsolva)
+            {
+                szabadMemoria -= programMeret;
+                sikeresMasolas = true;
+            } else
+            {
+                sikeresMasolas = false;
+            }
+
+            return sikeresMasolas;
+        }
+
+        public override string ToString()
+        {
+            string allapot = "";
+
+            if (bekapcsolva)
+            {
+                allapot += "A gép be van kapcsolva.";
+            } else
+            {
+                allapot += "A gép ki van kapcsolva.";
+            }
+
+            allapot += $" Szabad memória:{szabadMemoria}";
+            
+            return allapot;
+        }
+    }
+}
