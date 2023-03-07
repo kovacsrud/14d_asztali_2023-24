5baee293cf76a479084c206348510a5e3d15f5b2
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-03-07 09:09:29 +0100

    Fájl olvasás streamreader-el

---
+++ Fajlok/Fajlok/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fajlok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fajl = null;
+            StreamReader reader = null;
+            try
+            {
+                fajl = new FileStream("autoadat.csv", FileMode.Open);
+                reader = new StreamReader(fajl, Encoding.Default);
+                reader.ReadLine();//Első sor átlépése
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+
+                //Kivétel esetén elmarad a bezárás
+                //reader.Close();
+
+                    
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (reader!=null)
+                {
+                    reader.Close();
+                }
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
