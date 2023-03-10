6737c426e8ca80f491847e34ec539c2d995ca0bf
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-03-10 13:00:03 +0100

    Idojaras

---
+++ Idojaras/BitmapUsing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmapUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szamlalo = 0;
+
+            while (!Console.KeyAvailable)
+            {
+                using (var bitmap = new Bitmap(1280, 1280))
+                {
+                    Console.WriteLine(szamlalo);
+                    szamlalo++;
+                }
+                
+                
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -65,6 +66,56 @@ namespace Idojaras
             Console.WriteLine($"Legmelegebb nap:{MaxHomNap.Ev}.{MaxHomNap.Honap}.{MaxHomNap.Nap} {MaxHomNap.Ora}");
             Console.WriteLine($"Leghidegebb nap:{MinHomNap.Ev}.{MinHomNap.Honap}.{MinHomNap.Nap} {MinHomNap.Ora}");
 
+            //Összesítések
+            //Évenkénti átlaghőmérséklet
+            var evesAtlagHo = idojarasLista.IdojarasAdatok.ToLookup(x=>x.Ev).OrderBy(x=>x.Key);
+
+            foreach (var i in evesAtlagHo)
+            {
+                Console.WriteLine($"{i.Key} -> {i.Average(x=>x.Homerseklet)} Max:{i.Max(x=>x.Homerseklet)} Min:{i.Min(x=>x.Homerseklet)}");
+            }
+
+            //Átlaghőmérséklet Évenként és hónaponként
+
+            var haviAtlagHo = idojarasLista.IdojarasAdatok.ToLookup(x => new {x.Ev,x.Honap}).OrderBy(x=>x.Key.Ev).ThenBy(x=>x.Key.Honap);
+
+            foreach (var i in haviAtlagHo)
+            {
+                Console.WriteLine($"{i.Key.Ev}.{i.Key.Honap} {i.Average(x=>x.Homerseklet)}");
+            }
+
+            //Évenként, hónaponként, naponként
+
+            var napiAtlagHo = idojarasLista.IdojarasAdatok.ToLookup(x => new { x.Ev, x.Honap,x.Nap })
+                .OrderBy(x=>x.Key.Ev)
+                .ThenBy(x=>x.Key.Honap)
+                .ThenBy(x=>x.Key.Nap);
+
+            foreach (var i in napiAtlagHo)
+            {
+                Console.WriteLine($"{i.Key.Ev}.{i.Key.Honap}.{i.Key.Nap} {i.Average(x => x.Homerseklet)}");
+            }
+
+            try
+            {
+                FileStream fajl = new FileStream("statisztika.txt",FileMode.Create);
+
+                using (StreamWriter writer = new StreamWriter(fajl, Encoding.Default))
+                {
+                    writer.WriteLine($"ev;honap;nap;atlaghomerseklet");
+                    foreach (var i in napiAtlagHo)
+                    {
+                        writer.WriteLine($"{i.Key.Ev};{i.Key.Honap};{i.Key.Nap};{i.Average(x=>x.Homerseklet)}");
+                    }
+                }
+
+                Console.WriteLine("Kiírás kész!");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
 
             Console.ReadKey();
         }
