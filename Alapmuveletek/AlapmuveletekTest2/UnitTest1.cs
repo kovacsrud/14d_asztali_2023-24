f11ccd68f9da4708a3b8e396be534b8d8cb42432
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-03-20 08:34:23 +0100

    Tesztesetek fájlból

--- Alapmuveletek/AlapmuveletekTest2/UnitTest1.cs
+++ Alapmuveletek/AlapmuveletekTest2/UnitTest1.cs
@@ -1,5 +1,8 @@
 using NUnit.Framework;
 using Alapmuveletek;
+using System.Collections.Generic;
+using System.IO;
+using System;
 
 namespace AlapmuveletekTest2
 {
@@ -11,10 +14,11 @@ namespace AlapmuveletekTest2
         }
 
         [Test]
-        [TestCase(20,10,10)]
-        [TestCase(40, 20, 20)]
-        [TestCase(30, 20, 10)]
-        [TestCase(100, 50, 50)]
+        //[TestCase(20,10,10)]
+        //[TestCase(40, 20, 20)]
+        //[TestCase(30, 20, 10)]
+        //[TestCase(100, 50, 50)]
+        [TestCaseSource("GetOsszeadasAdatok")]
         public void OsszeadTest(double elvart,double a,double b)
         {
             //arrange -> elõkészület, pl. példányosítás
@@ -25,6 +29,19 @@ namespace AlapmuveletekTest2
             var sut = alapmuvelet.Osszead(a, b); //-> act
             Assert.AreEqual(elvart, sut); //->assert
             
+        }
+
+        public static IEnumerable<double[]> GetOsszeadasAdatok()
+        {
+            var sorok = File.ReadAllLines("tesztesetek_osszeadas.csv");
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                var e = sorok[i].Split(';');
+                var a = Convert.ToDouble(e[0]);
+                var b = Convert.ToDouble(e[1]);
+                var elvart = Convert.ToDouble(e[2]);
+                yield return new[] {elvart, a, b };
+            }
         }
 
 
