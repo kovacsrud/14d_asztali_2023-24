44f5a7fba835b333b71b604f6e7702497856bbdf
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-04-18 12:04:34 +0200

    Adatok WPF-en

---
+++ WpfAutoadatok/WpfAutoadatok/AutoLista.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WpfAutoadatok
+{
+    public class AutoLista
+    {
+        public List<Auto> Autok { get; set; }
+
+        public AutoLista(string fajl,char hatarolo,int start=1)
+        {
+            Autok = new List<Auto>();
+
+            var sorok = File.ReadAllLines(fajl, Encoding.UTF8);
+
+            for (int i = start; i < sorok.Length; i++)
+            {
+                Autok.Add(new Auto(sorok[i], hatarolo));
+            }
+
+        }
+    }
+}
---
+++ WpfAutoadatok/WpfAutoadatok/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAutoadatok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AutoLista autoLista;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "*.csv (csv fájlok)|*.csv";
+            if (dialog.ShowDialog()==true)
+            {
+                try
+                {
+                    autoLista = new AutoLista(dialog.FileName, ';');
+                    datagridAutoAdatok.ItemsSource = autoLista.Autok;
+                    tabitemAdatok.IsEnabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);                    
+                }
+                
+            }
+        }
+    }
+}
