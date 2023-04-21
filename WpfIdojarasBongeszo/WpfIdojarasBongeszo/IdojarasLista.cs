fe49ec9c5f495c38ff81b58f6fa470c75b395541
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-04-21 13:40:46 +0200

    Adatböngésző

---
+++ WpfIdojarasBongeszo/WpfIdojarasBongeszo/IdojarasLista.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfIdojarasBongeszo
+{
+    public class IdojarasLista
+    {
+        public List<Idojaras> IdojarasAdatok { get; set; }
+
+        public IdojarasLista(string fajl,char hatarolo,int start=1)
+        {
+            IdojarasAdatok = new List<Idojaras>();
+
+            var sorok = File.ReadAllLines(fajl,Encoding.Default);
+
+            for (int i = start; i < sorok.Length; i++)
+            {
+                IdojarasAdatok.Add(new Idojaras(sorok[i], ';'));
+            }
+        }
+
+        public List<int> GetEvek()
+        {
+            List<int> evek = new List<int>();
+            var evStat = IdojarasAdatok.ToLookup(x=>x.Ev).OrderBy(x=>x.Key);
+
+            foreach (var i in evStat)
+            {
+                evek.Add(i.Key);
+            }
+
+
+            return evek;
+        }
+
+        public List<int> GetHonapok(int ev)
+        {
+            List<int> honapok = new List<int>();
+            var aktualisEv = IdojarasAdatok.FindAll(x=>x.Ev==ev);
+            var honapStat = IdojarasAdatok.ToLookup(x => x.Honap).OrderBy(x=>x.Key);
+
+            foreach (var i in honapStat)
+            {
+                honapok.Add(i.Key);
+            }
+
+
+            return honapok;
+        }
+
+        public IOrderedEnumerable<Idojaras> GetAdatok(int ev)
+        {
+            var adatok = IdojarasAdatok.FindAll(x => x.Ev == ev).OrderBy(x=>x.Honap).ThenBy(x=>x.Nap).ThenBy(x=>x.Ora);
+
+            return adatok;
+        }
+    }
+}
---
+++ WpfIdojarasBongeszo/WpfIdojarasBongeszo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIdojarasBongeszo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IdojarasLista idojarasLista;
+        public MainWindow()
+        {
+            InitializeComponent();
+            idojarasLista = new IdojarasLista("idojaras.csv", ';');
+            listboxEvek.ItemsSource = idojarasLista.GetEvek();
+        }
+
+        private void listboxEvek_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            datagridIdojarasAdatok.ItemsSource = idojarasLista.GetAdatok((int)listboxEvek.SelectedItem);
+            listboxHonapok.ItemsSource = idojarasLista.GetHonapok((int)listboxEvek.SelectedItem);
+        }
+    }
+}
