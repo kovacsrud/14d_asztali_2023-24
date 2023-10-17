4ad4590c6d7c286876f48da1921442c40a4afa1a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-10-17 08:36:52 +0200

    Todo alkalmazás init

---
+++ WpfTodo/WpfTodo/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public bool Completed { get; set; }
+    }
+}
