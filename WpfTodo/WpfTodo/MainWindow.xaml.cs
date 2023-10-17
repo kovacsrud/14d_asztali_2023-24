f87941715af50aecc0a3a83f707ec2a6f7ee993a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2023-10-17 09:20:49 +0200

    WpfTodo

--- WpfTodo/WpfTodo/MainWindow.xaml.cs
+++ WpfTodo/WpfTodo/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfTodo.Models;
 
 namespace WpfTodo
 {
@@ -20,9 +23,42 @@ namespace WpfTodo
     /// </summary>
     public partial class MainWindow : Window
     {
+        TodoContext todocontext;
+        public ObservableCollection<Todo> Todos { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            todocontext = new TodoContext();
+            //todocontext.Add(new Todo { Title="Teszt",Completed=false});
+            //todocontext.SaveChanges();
+            todocontext.Todos.Load();
+            Todos = todocontext.Todos.Local.ToObservableCollection();
+            
+            DataContext = Todos;
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result=todocontext.SaveChanges();
+                if (result>0)
+                {
+                    datagridTodos.Items.Refresh();
+                    MessageBox.Show("Adatok mentve!");
+                } else
+                {
+                    MessageBox.Show("Nem történt mentés!");
+                }
+                
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
