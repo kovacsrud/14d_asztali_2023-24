43cf95f6cc7e72d6648907366f3a6cf09d1b0a18
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-02-07 13:15:55 +0100

    Konvertáló program

---
+++ WpfKonverter/WpfKonverter/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UnitsNet;
+using WpfKonverter.mvvm.viewmodel;
+
+namespace WpfKonverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new KonverterViewModel();
+        }
+
+        private void listboxMennyisegek_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var vm=(KonverterViewModel)DataContext;
+            if (listboxMennyisegek.SelectedItem!=null)
+            {
+                vm.KivalasztottMennyisegNev = listboxMennyisegek.SelectedItem.ToString();
+                vm.InputMertekegysegek = vm.MertekegysegBetoltes();
+                vm.OutputMertekegysegek = vm.MertekegysegBetoltes();
+                listboxInMertekegyseg.SelectedIndex = 0;
+                listboxOutMertekegyseg.SelectedIndex = 1;
+            }
+        }
+
+        private void listboxInMertekegyseg_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Konvertal();
+        }
+
+        private void listboxOutMertekegyseg_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Konvertal();
+        }
+        private void Konvertal()
+        {
+            var vm = (KonverterViewModel)DataContext;
+            if (listboxInMertekegyseg.SelectedItem!=null && listboxOutMertekegyseg.SelectedItem!=null && vm.InputErtek!=null)
+            {
+                vm.OutputErtek = UnitConverter.ConvertByName(vm.InputErtek, vm.KivalasztottMennyisegNev, vm.InputKivalasztott, vm.OutputKivalasztott);
+            }
+        }
+    }
+}
---
+++ WpfKonverter/WpfKonverter/mvvm/viewmodel/KonverterViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+using UnitsNet;
+
+namespace WpfKonverter.mvvm.viewmodel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class KonverterViewModel
+    {
+        public string KivalasztottMennyisegNev { get; set; }
+        public List<string> Mennyisegek { get; set; }
+        public List<string> InputMertekegysegek { get; set; }
+        public List<string> OutputMertekegysegek { get; set; }
+
+        public string InputKivalasztott { get; set; }
+        public string OutputKivalasztott { get; set; }
+        public double InputErtek { get; set; }
+        public double OutputErtek { get; set; }
+
+        public KonverterViewModel()
+        {
+              Mennyisegek=Quantity.Infos.Select(x=>x.Name).ToList();
+              KivalasztottMennyisegNev = Mennyisegek.FirstOrDefault();
+              InputMertekegysegek = MertekegysegBetoltes();
+              OutputMertekegysegek=MertekegysegBetoltes();
+              InputErtek = 1;
+        }
+
+        public List<string> MertekegysegBetoltes()
+        {
+            if (KivalasztottMennyisegNev!=null)
+            {
+                var mertekegysegek = Quantity.Infos.FirstOrDefault(x => x.Name == KivalasztottMennyisegNev).UnitInfos.Select(u => u.Name).ToList();
+
+                return new List<string>(mertekegysegek);
+            }
+            return new List<string>();
+        }
+
+    }
+}
